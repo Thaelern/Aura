c2a35df47c34c33c05cf250b94559505186fef95
Lasse B. I <lassebi92@gmail.com>
2023-09-24 11:52:36 +0200

    Player character and camera
    Set up basic movement and camera logic.

--- Source/Aura/Private/Character/AuraCharacter.cpp
+++ Source/Aura/Private/Character/AuraCharacter.cpp
@@ -3,3 +3,32 @@
 
 #include "Character/AuraCharacter.h"
 
+#include "Camera/CameraComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+
+AAuraCharacter::AAuraCharacter()
+{
+	/* Camera and SpringArm setup */
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>("SpringArm");
+	SpringArm->SetupAttachment(GetRootComponent());
+	MainPlayerCamera = CreateDefaultSubobject<UCameraComponent>("MainCamera");
+	MainPlayerCamera->SetupAttachment(SpringArm);
+
+	/* Basic settings for pawn behaviour. */
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = DefaultRotationRate;
+	GetCharacterMovement()->bConstrainToPlane = true;
+	GetCharacterMovement()->bSnapToPlaneAtStart = true;
+
+	/* Make sure the camera is fixed and does not not follow the pawn */
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationRoll = false;
+	bUseControllerRotationYaw = false;
+}
+
+void AAuraCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
--- Source/Aura/Public/Character/AuraCharacter.h
+++ Source/Aura/Public/Character/AuraCharacter.h
@@ -6,6 +6,9 @@
 #include "Character/AuraCharacterBase.h"
 #include "AuraCharacter.generated.h"
 
+class UCameraComponent;
+class USpringArmComponent;
+
 /**
  * 
  */
@@ -13,5 +16,23 @@ UCLASS()
 class AURA_API AAuraCharacter : public AAuraCharacterBase
 {
 	GENERATED_BODY()
+
+public:
+	AAuraCharacter(); // Constructor
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(EditAnywhere, Category = "Camera")
+	TObjectPtr<USpringArmComponent> SpringArm;
+
+
+	UPROPERTY(EditAnywhere, Category = "Camera")
+	TObjectPtr<UCameraComponent> MainPlayerCamera;
+
+	UPROPERTY(EditAnywhere, Category = "Movement")
+	FRotator DefaultRotationRate = FRotator(0.f, 400.f, 0.f);
+	
 	
 };
