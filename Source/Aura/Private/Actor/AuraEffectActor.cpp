9d62d9955d2ea0a2348954e1b1ff3b45aeff4b01
Lasse B. I. <79919868+Thaelern@users.noreply.github.com>
2025-01-22 12:59:02 +0100

    Set up functionality to remove dururation effects.

--- Source/Aura/Private/Actor/AuraEffectActor.cpp
+++ Source/Aura/Private/Actor/AuraEffectActor.cpp
@@ -2,7 +2,6 @@
 
 
 #include "Actor/AuraEffectActor.h"
-
 #include "AbilitySystemComponent.h"
 #include "AbilitySystemBlueprintLibrary.h"
 
@@ -25,8 +24,65 @@ void AAuraEffectActor::ApplyEffectToTarget(AActor* TargetActor, TSubclassOf<UGam
 	FGameplayEffectContextHandle EffectContextHandle =  TargetAbilitySystemComponent->MakeEffectContext();
 	EffectContextHandle.AddSourceObject(this);
 	FGameplayEffectSpecHandle  EffectSpecHandle = TargetAbilitySystemComponent->MakeOutgoingSpec(GameplayEffectToApplyClass, 1.0f, EffectContextHandle );
-	TargetAbilitySystemComponent->ApplyGameplayEffectSpecToSelf(*EffectSpecHandle.Data.Get());
+	FActiveGameplayEffectHandle ActiveEffectHandle = TargetAbilitySystemComponent->ApplyGameplayEffectSpecToSelf(*EffectSpecHandle.Data.Get());
+
+	const bool bIsInfinite = EffectSpecHandle.Data.Get()->Def.Get()->DurationPolicy == EGameplayEffectDurationType::Infinite;
+	if (bIsInfinite && InfiniteEffectRemovalPolicy == EEffectRemovelPolicy::RemoveOnEndOverlap)
+	{
+		ActiveEffectHandles.Add(ActiveEffectHandle, TargetAbilitySystemComponent);
+	}
 		
+}
+
+void AAuraEffectActor::OnOverlap(AActor* TargetActor)
+{
+	if (InstantEffectApplicationPolicy == EEffectApplicationPolicy::ApplyOverlap)
+	{
+		ApplyEffectToTarget(TargetActor, InstantGameplayEffect);
+	}
+	if (DurationGameplayEffectPolicy == EEffectApplicationPolicy::ApplyOverlap)
+	{
+		ApplyEffectToTarget(TargetActor, DurationGameplayEffect);
+	}
+	if (InfiniteGameplayEffectPolicy == EEffectApplicationPolicy::ApplyOverlap)
+	{
+		ApplyEffectToTarget(TargetActor, InfiniteGameplayEffect);
+	}
+}
+
+void AAuraEffectActor::OnEndOverlap(AActor* TargetActor)
+{
+	if (InstantEffectApplicationPolicy == EEffectApplicationPolicy::ApplyOnEndOverlap)
+	{
+		ApplyEffectToTarget(TargetActor, InstantGameplayEffect);
+	}
+	if (DurationGameplayEffectPolicy == EEffectApplicationPolicy::ApplyOnEndOverlap)
+	{
+		ApplyEffectToTarget(TargetActor, DurationGameplayEffect);
+	}
+	if (InfiniteGameplayEffectPolicy == EEffectApplicationPolicy::ApplyOverlap)
+	{
+		ApplyEffectToTarget(TargetActor, InfiniteGameplayEffect);
+	}
+	if (InfiniteEffectRemovalPolicy == EEffectRemovelPolicy::RemoveOnEndOverlap)
+	{
+		UAbilitySystemComponent* TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(TargetActor);
+		if (!IsValid(TargetASC)) return;
+
+		TArray<FActiveGameplayEffectHandle> HandlesToRemove;
+		for (auto HandlePair : ActiveEffectHandles)
+		{
+			if ( TargetASC == HandlePair.Value)
+			{
+				TargetASC->RemoveActiveGameplayEffect(HandlePair.Key);
+				HandlesToRemove.Add(HandlePair.Key);
+			}
+		}
+		for (auto& Handle : HandlesToRemove)
+		{
+			ActiveEffectHandles.FindAndRemoveChecked(Handle);
+		}
+	}
 }
 
 
--- Source/Aura/Public/Actor/AuraEffectActor.h
+++ Source/Aura/Public/Actor/AuraEffectActor.h
@@ -4,10 +4,27 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "GameplayEffectTypes.h"
 #include "AuraEffectActor.generated.h"
 
 
 class UGameplayEffect;
+class UAbilitySystemComponent;
+
+UENUM(BlueprintType)
+enum class EEffectApplicationPolicy
+{
+	ApplyOverlap,
+	ApplyOnEndOverlap,
+	DoNotApply
+};
+
+UENUM(BlueprintType)
+enum class EEffectRemovelPolicy
+{
+	RemoveOnEndOverlap,
+	DoNotRemove
+};
 
 UCLASS()
 class AURA_API AAuraEffectActor : public AActor
@@ -21,6 +38,15 @@ protected:
 
 	UFUNCTION(Blueprintable, BlueprintCallable)
 	void ApplyEffectToTarget(AActor* TargetActor, TSubclassOf<UGameplayEffect> GameplayEffectToApplyClass);
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "GAS | Applied Effects");
+	bool bDestroyOnEffectRemoval = false;
+
+	UFUNCTION(BlueprintCallable)
+	void OnOverlap(AActor* TargetActor);
+
+	UFUNCTION(BlueprintCallable)
+	void OnEndOverlap(AActor* TargetActor);
 	
 	// TSubClassOf means we are setting the class from BP, but using it in code.
 	// Same concept we do with UI / Widgets
@@ -28,10 +54,24 @@ protected:
 	TSubclassOf<UGameplayEffect> InstantGameplayEffect;
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "GAS | Applied Effects");
+	EEffectApplicationPolicy InstantEffectApplicationPolicy = EEffectApplicationPolicy::ApplyOverlap;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "GAS | Applied Effects");
 	TSubclassOf<UGameplayEffect> DurationGameplayEffect;
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "GAS | Applied Effects");
+	EEffectApplicationPolicy DurationGameplayEffectPolicy = EEffectApplicationPolicy::ApplyOverlap;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "GAS | Applied Effects");
 	TSubclassOf<UGameplayEffect> InfiniteGameplayEffect;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "GAS | Applied Effects");
+	EEffectApplicationPolicy InfiniteGameplayEffectPolicy = EEffectApplicationPolicy::ApplyOverlap;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "GAS | Applied Effects");
+	EEffectRemovelPolicy InfiniteEffectRemovalPolicy = EEffectRemovelPolicy::RemoveOnEndOverlap;
+
+	TMap<FActiveGameplayEffectHandle, UAbilitySystemComponent*> ActiveEffectHandles;
 	
 
 	
